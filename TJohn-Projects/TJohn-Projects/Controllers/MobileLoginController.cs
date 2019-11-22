3d0fbfbb4965c7a874acbe170e0a8c7fdc76205f
jivinj1 <jivinj1@hotmail.com>
2019-11-22 21:24:31 +0530

    MobileLogin

---
+++ TJohn-Projects/TJohn-Projects/Controllers/MobileLoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TJohn_Projects.Controllers
+{
+    public class MobileLoginController : Controller
+    {
+        // GET: MobileLogin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
